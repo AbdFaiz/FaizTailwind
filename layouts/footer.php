a283457c195661965a39a208583f19a9f63c5eab
AbdFaiz <abdurrahmanfaiz187@gmail.com>
2023-09-02 21:28:43 +0700

    Tailwind php

---
+++ layouts/footer.php
@@ -0,0 +1,3 @@
+<!-- footer -->
+</div>
+<div class="bg-slate-900 text-center p-3 text-white">© CopyHitam Warkop</div>
---
+++ layouts/header.php
@@ -0,0 +1,8 @@
+<!-- header -->
+<nav class="bg-slate-900 p-3 flex justify-between items-center">
+        <div class="text-teal-300 font-semibold text-2xl ms-3">Class Rank Tailwind</div>
+        <div class="flex me-3">
+          <input type="text" class="px-3 py-2 rounded-l-md focus:outline-none" placeholder="Search">
+          <button class="bg-teal-200 hover:bg-teal-700 hover:text-white px-4 py-2 rounded-r-md transition-all duration-200">Search</button>
+        </div>
+      </nav>
